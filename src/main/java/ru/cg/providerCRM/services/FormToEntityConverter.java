23e57c97fbd7e3e94e27eba8c2922487be31e064
Ackar <Ackar94@gmail.com>
2015-08-03 20:58:17 +0200

    converter added

---
+++ src/main/java/ru/cg/providerCRM/services/FormToEntityConverter.java
@@ -0,0 +1,33 @@
+package ru.cg.providerCRM.services;
+
+import ru.cg.providerCRM.entity.Employee;
+import ru.cg.providerCRM.web.form.EmployeeEditForm;
+
+public class FormToEntityConverter {
+
+    public static Employee EmployeeFormToEmployee(EmployeeEditForm form,
+                                           ProviderService providerService,
+                                           ProducerService producerService) {
+        Employee employee = new Employee();
+        employee.setId(form.getId());
+        employee.setFullName(form.getFullName());
+        employee.setEmail(form.getEmail());
+        employee.setHomePhoneNumber(form.getHomePhoneNumber());
+        employee.setWorkPhoneNumber(form.getWorkPhoneNumber());
+        employee.setPosition(form.getPosition());
+        if (form.getProviderId() != 0){
+            employee.setProvider(providerService.getById(form.getProviderId()));
+        } else {
+            System.out.println(form.getProviderId());
+            employee.setProvider(null);
+        }
+
+        if (form.getProducerId() != 0){
+            employee.setProducer(producerService.getById(form.getProducerId()));
+        } else {
+            employee.setProducer(null);
+        }
+        return employee;
+    }
+
+}
